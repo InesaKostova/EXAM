4f1da90749d131d25d082b7bfd5aac33a525e80b
Inesa Kostova <kostovainesa@gmail.com>
2025-03-12 11:00:13 +0200

    Add files via upload

---
+++ Fruit Market.cs
@@ -0,0 +1,15 @@
+﻿double strawberryPrice = double.Parse(Console.ReadLine());
+double countBanana=double.Parse(Console.ReadLine());
+double countOrange = double.Parse(Console.ReadLine());
+double countBerry= double.Parse(Console.ReadLine());
+double countStrawberry = double.Parse(Console.ReadLine());
+
+
+double berryPrice = strawberryPrice / 2;
+double orangePrice = berryPrice - (berryPrice * 0.4);
+double bananaPrice = berryPrice - (berryPrice * 0.8);
+
+double sum=(countStrawberry*strawberryPrice)+(countBanana*bananaPrice)+(countOrange*orangePrice)+(countBerry*berryPrice);
+
+
+Console.WriteLine($"{sum:F2}");
